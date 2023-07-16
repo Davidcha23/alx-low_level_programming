9e6dab4aef3aeb551856ecc71c7a4c09903f351d
Davidcha23 <dkchaachaa@gmail.com>
2023-07-16 10:00:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,9 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints a line with function prinf
+ * Return: 0
+ */
+int main(void)
+{
+prinf("\"Programming is like building a multilingual puzzle");
+return (0);
